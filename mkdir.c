9dc46e73ca5e8f774a8e40e754c0f7b13bfee320
jiwonkim1215 <winner_j@naver.com>
2013-07-15 13:30:40 +0900

    Create mkdir.c

---
+++ mkdir.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]){
+  if(argc<2){
+		printf("usage: mkdir directoryName\n");
+		exit(0);
+	}
+	if(mkdir(argv[1],0755) != 0)
+		printf("failure");
+}
